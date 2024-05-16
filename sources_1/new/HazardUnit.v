96c485b47d5eab47b89f303bdf1cea7b0ec7533a
TAIDP <taidp.vn@gmail.com>
2024-05-16 09:38:08 +0700

    update

--- sources_1/new/HazardUnit.v
+++ sources_1/new/HazardUnit.v
@@ -6,6 +6,8 @@ module HazardUnit(
     output [1:0]    hazardSelA, hazardSelB
     );
     
+        assign hazardSelA = 0;
+        assign hazardSelB = 0;
     assign          hazardSelA = (!reset_n)?2'b00:
                                  ((RegWEn_MA == 1'b1) & (RD_MA == RS1_EX))?2'b01:
                                  ((RegWEn_WB == 1'b1) & (RD_WB == RS1_EX))?2'b10:2'b00;
@@ -13,23 +15,3 @@ module HazardUnit(
                                  ((RegWEn_MA == 1'b1) & (RD_MA == RS2_EX))?2'b01:
                                  ((RegWEn_WB == 1'b1) & (RD_WB == RS2_EX))?2'b10:2'b00;
 endmodule
-
-
-//module Hazard_unit(
-//    input reset_n, RegWriteM, RegWriteW,RegWriteW_delay,
-//    input [4:0] RD_M, RD_W,RD_W_delay, Rs1_E, Rs2_E,
-//    output [1:0] ForwardAE, ForwardBE );
-  
-    
-    
-//    assign ForwardAE = (reset_n == 1'b0) ? 2'b00 : 
-//                       ((RegWriteM == 1'b1)  & (RD_M == Rs1_E)) ? 2'b10 :
-//                       ((RegWriteW == 1'b1)  & (RD_W == Rs1_E)) ? 2'b01 : 
-//                       ((RegWriteW_delay == 1'b1)  & (RD_W_delay == Rs1_E)) ? 2'b11 :2'b00;
-                       
-//    assign ForwardBE = (reset_n == 1'b0) ? 2'b00 : 
-//                       ((RegWriteM == 1'b1)  & (RD_M == Rs2_E)) ? 2'b10 :
-//                       ((RegWriteW == 1'b1)  & (RD_W == Rs2_E)) ? 2'b01 : 
-//                       ((RegWriteW_delay == 1'b1)  & (RD_W_delay == Rs2_E)) ? 2'b11 :2'b00;
-
-//endmodule
